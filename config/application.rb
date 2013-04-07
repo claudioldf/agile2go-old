0ea6fdd26b1be5bfade5c0f8af29cd7d748f94ea
rafaeljesus <rafael_jesus86hotmail.com>
2013-04-06 21:10:39 -0300

    fixing heroku problem1

--- config/application.rb
+++ config/application.rb
@@ -78,5 +78,7 @@ module Agile
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
