aa4c1a4a93a841eb4aff98a5c514a9db6801c4be
rafaeljesus <rafael_jesus86hotmail.com>
2013-04-07 01:54:20 -0300

    trying to fix sign up page4 - devise routes problem5

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Agile::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
-  devise_for :users, :controllers => { :registration => 'registration' }, :path => "auth",
+  devise_for :users, :controllers => { :registration => 'registration', :session => "session" }, :path => "auth",
   :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' 
   #devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
   resources :users
