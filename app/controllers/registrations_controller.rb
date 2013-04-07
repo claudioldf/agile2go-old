b23450580d32944dbffdeb9ece74798cce403188
rafaeljesus <rafael_jesus86hotmail.com>
2013-04-07 16:59:24 -0300

    trying to fix sign up page - devise routes problem10

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
-
-  def new
-    @user = User.new    
-  end
-
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Agile::Application.routes.draw do
   resources :sprints
   resources :projects
 
-  authenticated :user do
-    #root :to => 'home#index'
-    match 'users' => 'home#index', :as => 'user_root'
+  authenticated :user do    
+    root :to => "home#index"  
+    #match 'users' => 'home#index', :as => 'user_root'
+  end
+  devise_scope :user do
+    get "sign_up", :to => "devise/resgistrations#new"
   end
   root :to => "home#index"  
-  devise_for :users, :controllers => { :registration => 'users' }, :path => "auth"  
-  #devise_for :users, :path => "auth", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
+  devise_for :users, :path => "auth"    
   resources :users
 end
