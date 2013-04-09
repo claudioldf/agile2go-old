6683692f44503d9e121be2f5aa157176e5eedaf6
rafaeljesus <rafael_jesus86hotmail.com>
2013-04-09 00:51:35 -0300

    problem deploying on heroku: devise3

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Agile::Application.config.secret_token = '41aa79780d2bc269d5ed6a149e160314c6f3108e72a2ae51135f877018560aefa410ff70715cbc697dafe5ecbd9ec53eac6fe6a9a52dbf7d8fae46b96f207a5d'
+Agile::Application.config.secret_token = '41aa79780d2bc269d5ed6a149e160314c6f3108e72a2ae51135f877018560aefa410ff70715cbc697dafe5ecbd9ec53eac6fe6a9a52dbf7d8fae46b96f207a5dasx'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Agile::Application.config.session_store :cookie_store, key: '_agile_session'
+Agile::Application.config.session_store :cookie_store, key: '_agile2go_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
