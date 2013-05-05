d0c1fb84bb9d30dc9aac0c31c69e80376cd5a266
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-05-05 14:12:40 -0300

    #try2 to fix charts on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Agile::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
+  config.assets.precompile += ['rollover.js', 'highcharts.js']
+
   # Generate digests for assets URLs
   config.assets.digest = true
 
