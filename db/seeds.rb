8eb55cd30dfbe6df495ffb95379bf477df6bc327
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-05-05 13:35:43 -0300

    seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,48 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) are set in the file config/application.yml.
 # See http://railsapps.github.com/rails-environment-variables.html
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
+#puts 'ROLES'
+#YAML.load(ENV['ROLES']).each do |role|
+# Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+#  puts 'role: ' << role
+#end
+#puts 'DEFAULT USERS'
+#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+#puts 'user: ' << user.name
+#user.add_role :admin
+
+12.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 3, hours: "#{i}:00", status: "TODO", sprint_id: 1)
 end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
-user.add_role :admin
+
+6.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 2, hours: "#{i}:00", status: "ONGOING", sprint_id: 1)
+end
+
+3.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 2, hours: "#{i}:00", status: "DONE", sprint_id: 1)
+end
+
+32.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 3, hours: "#{i}:00", status: "TODO", sprint_id: 2)
+end
+
+6.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 2, hours: "#{i}:00", status: "ONGOING", sprint_id: 2)
+end
+
+2.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 2, hours: "#{i}:00", status: "DONE", sprint_id: 2)
+end
+
+6.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 3, hours: "#{i}:00", status: "TODO", sprint_id: 3)
+end
+
+2.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 2, hours: "#{i}:00", status: "ONGOING", sprint_id: 3)
+end
+
+1.times do |i|
+  Task.create(storie: "As a SCRUM MASTER i want to delete any project", priority: 2, hours: "#{i}:00", status: "DONE", sprint_id: 3)
+end
