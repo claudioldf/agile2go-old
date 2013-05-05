71183aa26e11487f06a86a4b872c5ba060b6efdb
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-05-05 18:43:51 -0300

    #try10 to fix charts on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -91,4 +91,8 @@ $(document).on("focus", "[data-behaviour~='datepicker']", function(e){
     $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true})
 });
 
+$(document).on("load", "[div~='chart']", function(e){
+    
+});
+
         
