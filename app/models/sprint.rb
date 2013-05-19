b591faa720da59b4a16fb67af26c36ee59806de4
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-05-19 00:28:45 -0300

    fixing nil on tasks index page

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,6 +1,6 @@
 class Sprint < ActiveRecord::Base
   belongs_to :project  
-  attr_protected :daily_scrum, :end_date, :goal, :name, :start_date, :project_id
+  attr_accessible :daily_scrum, :end_date, :goal, :name, :start_date, :project_id
   validates_presence_of :daily_scrum, :end_date, :goal, :name, :start_date, :project_id
 
   def self.to_csv(options = {})
