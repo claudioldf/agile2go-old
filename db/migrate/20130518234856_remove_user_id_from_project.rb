8895a7fd1c6943cec9c6834182a4a1ceea34f05c
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-05-19 00:01:29 -0300

    seed file not working cuz migrate file

--- db/migrate/20130518234856_remove_user_id_from_project.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromProject < ActiveRecord::Migration
-  def up
-    remove_column :projects, :user_ids
-  end
-
-  def down
-    add_column :projects, :user_ids, :integer
-  end
-end
