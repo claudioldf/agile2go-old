3cbd4f21c76daec3bfa575c3a6410dc2b68ce3a5
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-05-21 08:13:35 -0300

    home page layout enhancement

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
-	has_many :users  
+	has_many :users
+  has_many :sprints  
   accepts_nested_attributes_for :users, :allow_destroy => true  
+  accepts_nested_attributes_for :sprints, :allow_destroy => true  
   attr_protected :company, :description, :name
   validates_presence_of :company, :description, :name
 
