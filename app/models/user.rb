a8278a82fd5493d119bce47e72f076426c3d5918
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-06-10 00:25:00 -0300

    fixed: exporting users are ok on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,6 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :avatar_url, :project_id, :task_id
   validates_presence_of :name, :email
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-
-  def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv << %w(Name Email Registered Role)
-        all.each do |user|
-        csv << [user.name, user.email, user.created_at.to_date, user.roles.first.name]
-      end
-    end
-  end
   
   private
 
@@ -29,5 +20,16 @@ class User < ActiveRecord::Base
 	def create_avatar_url
    	 self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
 	end  
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << %w(Name Email Registered Role)
+        all.each do |user|
+        csv_header = [user.name, user.email, user.created_at.to_date]         
+        csv_header << user.roles.first.name unless user.roles.empty?                  
+        csv << csv_header
+      end
+    end
+  end
 
 end
