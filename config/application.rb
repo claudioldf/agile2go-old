8c1bce18b443fdfe9ffa7688037779e26c1fcb2e
Rafael <rafael@rafael-VPCEA36FX.(none)>
2013-06-19 19:27:24 -0300

    js is not pre-comiled says heroku

--- config/application.rb
+++ config/application.rb
@@ -76,7 +76,7 @@ module Agile
     config.active_record.whitelist_attributes = false
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    #config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
