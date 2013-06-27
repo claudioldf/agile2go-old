2a093d5411b438a27bbc2dd8a39dcf6cd01d85f9
Rafael Jesus <rafael_jesus86@hotmail.com>
2013-06-26 23:57:56 -0300

    validation added: user and email from user are uniq now

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,10 @@ class User < ActiveRecord::Base
   attr_accessible :role_ids, :as => :master  
   attr_accessible :user_ids, :as => :master  
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :avatar_url, :project_id, :task_id
-
-  validates_presence_of :name, :email
+  
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates :name, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :slug, uniqueness: true, presence: true, exclusion: {in: %w[signup login]}
   
   def generate_slug
