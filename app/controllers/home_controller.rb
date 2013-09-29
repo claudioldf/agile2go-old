4c2dd91668f528a962234453978e5063e0877efb
Rafael Jesus <rafael_jesus86hotmail.com>
2013-09-29 00:11:28 -0300

    implemented stale? at index home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
+  respond_to :html
+
   def index
-    @projects = Project.all
+    @projects = Project.ordered
     @tasks = Task.names
+    if stale? etag: @projects.all, last_modified: @projects.maximum(:updated_at)
+      respond_with @projects
+    end
   end
 end
