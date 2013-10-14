33e0e7744b4cecf12820bac705dd2ae00c145862
Rafael Jesus <rafael_jesus86hotmail.com>
2013-10-13 21:39:58 -0300

    refactoring project_spec and adding more tests on sprint_spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe Project, 'attributes' do
-  context 'validate presence of attibutes' do
+  context 'validate attibutes' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:company) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:slug) }
-  end
 
-  it "create a new project" do
-    project = FactoryGirl.create(:project)
-    project.should be_valid
+    it "create a new project" do
+      project = FactoryGirl.create(:project)
+      project.should be_valid
+    end
   end
 end
 
@@ -23,19 +23,46 @@ describe Project, 'associations' do
   it { should accept_nested_attributes_for(:sprints).allow_destroy(true) }
 end
 
+describe Project, 'public methods' do
+  let(:p) { FactoryGirl.create(:project) }
+
+  it 'generate slug' do
+    expect(p.slug).to eq 'project-test'
+  end
+end
+
 describe Project, 'queries' do
+  context 'scopes' do
+    it 'return project names' do
+      expect(Project.names).to be_true
+    end
+
+    it 'return project ordered' do
+      expect(Project.ordered).to be_true
+    end
+  end
+
   context 'full-text search' do
     let(:p) { FactoryGirl.create(:project) }
+
     it 'search by name' do
-      Project.search(p.name).should be_true
+     result =  Project.search(p.name)
+     expect(result.size).to eq 1
     end
 
     it 'search by campany' do
-      Project.search(p.company).should be_true
+      result = Project.search(p.company)
+      expect(result.size).to eq 1
     end
 
     it 'search by description' do
-      Project.search(p.description).should be_true
+      result = Project.search(p.description)
+      expect(result.size).to eq 1
+    end
+
+    it 'search by non-existing value' do
+      result = Project.search('non-existing')
+      expect(result.size).to eq 0
     end
   end
 end
--- spec/models/sprint_spec.rb
+++ spec/models/sprint_spec.rb
@@ -1,21 +1,29 @@
 require 'spec_helper'
 
 describe Sprint, 'attributes' do
-  context 'validate presence of attributes' do
+  context 'validate attributes' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:start_date) }
     it { should validate_presence_of(:project_id) }
     it { should allow_value(Date.today).for(:start_date) }
     it { should allow_value(Date.today).for(:end_date) }
-  end
 
-  it "create a new sprint" do
-    sprint = FactoryGirl.create(:sprint)
-    sprint.should be_valid
+    it "create a new sprint" do
+      sprint = FactoryGirl.create(:sprint)
+      sprint.should be_valid
+    end
   end
 end
 
 describe Sprint, 'associations' do
   it { should have_many(:tasks) }
   it { should belong_to(:project) }
+end
+
+describe Sprint, 'queries' do
+  context 'scopes' do
+    it 'return sprint ordered' do
+      expect(Sprint.ordered).to be_true
+    end
+  end
 end
